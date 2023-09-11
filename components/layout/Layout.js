a1160a817480defcde4916709b6c526218df5953
Shahin <hawk.azizi1@gmail.com>
2023-09-11 22:56:32 +0330

    add stuff

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -6,17 +6,11 @@ function Layout({ children }) {
     <>
       <header className={styles.header}>
         <Link href="/">
-          <h2>BOTOCAR</h2>
+          <h2>Stock Cars</h2>
           <p>Choose and Buy your car</p>
         </Link>
       </header>
       <div className={styles.container}>{children}</div>
-      <footer className={styles.footer}>
-        <a href="https://botostart.ir" target="_blank">
-          Botostart
-        </a>{" "}
-        Next.js courses | BotoCar Project &copy;
-      </footer>
     </>
   );
 }
